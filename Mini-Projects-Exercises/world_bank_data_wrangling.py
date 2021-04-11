344547e5a6b3f3201fb42a2bc329931851a52433
Chaudhari, Dheeraj <dheeraj.chaudhari2@thermofisher.com>
2021-04-11 12:57:17 -0700

    4.4 Mini-Project: JSON-Based Data Exercise - Alternative

--- Mini-Projects-Exercises/world_bank_data_wrangling.py
+++ Mini-Projects-Exercises/world_bank_data_wrangling.py
@@ -55,15 +55,31 @@ print(wb_json_df)
 
 #1. Find the 10 countries with most projects
 print(wb_json_df.groupby('countryshortname').size().sort_values(ascending=False)[:10])
+# print(wb_json_df.groupby('countryshortname')['project_name'].count().nlargest(10))
 
 #2. Find the top 10 major project themes (using column 'mjtheme_namecode')
-#json_normalize(sample_json_df.mjtheme_namecode[0])
+json_normalize(wb_json_df.mjtheme_namecode[0])
+
 df = pd.DataFrame(columns=['name', 'code'])
 for index, row in wb_json_df.iterrows():
     append_me = json_normalize(row.mjtheme_namecode)
     df = pd.concat([df, append_me])
 
 print(df.isnull().sum())
+
+# OR
+# # load json file w/ context manager
+# with open('data/world_bank_projects.json') as file:
+#     json_obj = json.load(file)
+#
+# # normalize the mjtheme_namecode columns using json_normalize
+# df_mjtheme_namecode = json_normalize(json_obj, 'mjtheme_namecode')
+# df_mjtheme_namecode.head()
+# group by name
+# df_mjtheme_namecode['name'].value_counts().nlargest(10)
+
+# Since there're missing names, try group by code instead
+# df_mjtheme_namecode['code'].value_counts().nlargest(10)
 
 # In 2. above you will notice that some entries have only the code and the name is missing. Create a dataframe with the missing names filled in.
 import numpy as np
@@ -74,3 +90,31 @@ temp = filled_wb_df.dropna().drop_duplicates()
 final = filled_wb_df.merge(temp, on="code", how="left", suffixes=("_old", "_new")).drop(columns=["name_old"])
 print(final.shape)
 print(temp.shape)
+
+
+# OR
+
+# # Collect name for each code
+# code_name_dict = {}
+#
+# for code, name in zip(df_mjtheme_namecode['code'], df_mjtheme_namecode['name']):
+#     if name != None and name != '' and code not in code_name_dict:
+#         code_name_dict[code] = name
+#
+# print(code_name_dict)
+#
+# # Fill missing name using the name collected in code_name_dict
+# df_mjtheme_namecode['name'] = df_mjtheme_namecode['code'].map(code_name_dict)
+# df_mjtheme_namecode.head()
+#
+# # Double check by group by name again & confirm that there're no missing names
+# # and the results are same as group by code as in question #2
+# df_mjtheme_namecode['name'].value_counts().nlargest(10)
+#
+# # Plot barchart after filling missing names
+# import matplotlib.pyplot as plt
+#
+# plot = df_mjtheme_namecode['name'].value_counts().plot(kind='bar', title='Top 10 Major Project Themes', rot=90)
+# plot.set_xlabel("Project Themes")
+# plot.set_ylabel("Project Count")
+# plt.show()
